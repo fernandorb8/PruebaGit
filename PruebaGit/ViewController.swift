97f3cf61fd6acb4e698f3d2cb4df1836b6204fba
Fernando Reyes Bejarano <fernando@adalbertos-macbook-2.local>
2019-02-10 18:02:48 -0500

    Initial Commit

---
+++ PruebaGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PruebaGit
+//
+//  Created by Fernando Reyes Bejarano on 2/10/19.
+//  Copyright © 2019 Fernando Reyes Bejarano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
